dd5882a9dec544abeb62ab5f78f956d17ee494e9
root <root@localhost>
2023-02-14 16:13:39 +0800

    20230214

---
+++ unlockcopy.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         取消不可选择
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http*://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @grant        GM_openInTab
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_notification
+// @grant        window.onurlchange
+// ==/UserScript==
+
+(function() {
+    'use strict';
+         GM_addStyle(' * {user-select: text !important}')
+    // Your code here...
+})();
